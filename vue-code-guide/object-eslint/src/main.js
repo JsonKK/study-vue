c8e24e365d3dbaf3d69eea1e4b80d1eb59a3354c
jsonKK <595675516@qq.com>
2019-05-15 17:57:15 +0800

    增加eslint项目

---
+++ vue-code-guide/object-eslint/src/main.js
@@ -0,0 +1,15 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import App from './App'
+const config = require('../config')
+const lint = "eslint"
+console.log(lint);
+let a = process.env.NODE_ENV
+Vue.config.productionTip = false
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  components: { App },
+  template: '<App/>'
+})
