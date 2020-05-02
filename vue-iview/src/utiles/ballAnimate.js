b6c4b9942e5bf9bbed0fa08aa1097aa53859f37d
JsonKK <18259002260@163.com>
2020-05-02 16:24:52 +0800

    小球动画功能实现

--- vue-iview/src/utiles/ballAnimate.js
+++ vue-iview/src/utiles/ballAnimate.js
@@ -1,25 +1,76 @@
-const  ball = class Ball{
-  constructor(id){
-    this.init(id)
-  }
-  //获取画布相关信息
-  getCanvasInfo(id){
-    if(!id || !document.getElementById(id)){
-      return false
+const ball = class Ball {
+    constructor({ pic }) {
+        console.log(pic)
+        this.init(pic)
     }
-    let obj = {};
-    
-  }
-  //初始化
-  init(content){
-    
-    this.x = 'xxx';
-    this.random()
-  }
 
-  random(min,max){
-    return Math.random()*(max-min) + min;
-  }
+    //初始化
+    init(con) {
+        let picDom = document.getElementById(con);
+        let colors = this.getColors();
+        this.picDom = picDom;
+        this.picWidth = picDom.width;
+        this.picHeight = picDom.height;
+        this.r = this.random(1, this.picHeight < this.picWidth ? this.picHeight / 30 : this.picWidth / 30);
+        this.x = this.random(this.r, this.picWidth-this.r);
+        this.y = this.random(this.r, this.picHeight-this.r);
+        this.xV = this.random(0,2,true);
+        this.yV = this.random(0,1,true);
+        this.color = colors[this.random(0,colors.length-1)];
+        this.xAdd = true;
+        this.yAdd = true;
+        this.draw();
+    }
+
+    draw(){
+        let content = this.picDom.getContext('2d');
+        let {x,y,r,picDom,color} = this;
+        // content.clearRect(0,0,picDom.width,picDom.height);
+        content.beginPath();
+        content.fillStyle = color;
+        content.arc(x,y,r,0,Math.PI*2);
+        content.fill();
+    }
+
+    clearRect(){
+        let content = this.picDom.getContext('2d');
+        let {picDom} = this;
+        content.clearRect(0,0,picDom.width,picDom.height);
+    }
+
+    move(){
+        let {x,y,xV,yV,picWidth,picHeight,r} = this;
+        if(x+xV >= picWidth - r){
+            this.xAdd = false;
+        }
+        if(x-xV <= 0 + r){
+            this.xAdd = true;
+        }
+        this.xAdd ? this.x += xV : this.x -= xV;
+        if(y+yV >= picHeight - r){
+            this.yAdd = false;
+        }
+        if(y-yV <= 0 + r){
+            this.yAdd = true;
+        }
+        this.yAdd ? this.y += yV : this.y -= yV;
+        this.draw();
+    }
+
+    //颜色颜色
+    getColors(){
+        return ['#FFFFCC','#FF6666','#FFFF00','#0066CC','#336633','#99CC66','#0099CC','#FF6666','#CCCCCC']
+    }
+
+    //返回随机数
+    random(min, max,isFloat) {
+        if(isFloat){
+            return Math.random() * (max - min) + min;
+        }
+        else{
+            return Math.ceil(Math.random() * (max - min) + min);
+        }
+    }
 }
 
-export {ball};
+export { ball };
--- vue-iview/src/views/main/canvasBall.vue
+++ vue-iview/src/views/main/canvasBall.vue
@@ -1,52 +1,54 @@
 <template>
-  <div id="canvas-ball">
-    <canvas class="ball-box" ></canvas>
-  </div>
+    <div id="canvas-ball">
+        <canvas class="ball-box" id="ball-box"></canvas>
+    </div>
 </template>
  
 <script>
-  import {ball} from '../../utiles/ballAnimate.js';
-  export default {
+import { ball } from '../../utiles/ballAnimate.js';
+export default {
     name: 'canvasBall',
     components: {},
-    data(){
-      return{}
+    data() {
+        return {
+            ballNum : 200
+        }
     },
-    mounted(){
-      this.initBall();
-      let renderBall = new ball();
-      
+    mounted() {
+        this.initBall();
+
     },
-    methods:{
-      initBall(){
-        let canvas = document.getElementsByClassName('ball-box')[0];
-        let canvasBox = document.getElementById('canvas-ball');
-        canvas.width = canvasBox.clientWidth;
-        canvas.height = 600;
-        let content = canvas.getContext('2d');
-        //绘制动画
-        let x = 50,y=50;
-        let animate = setInterval(()=>{
-          content.clearRect(0,0,canvas.width,canvas.height);
-          content.beginPath();
-          content.fillStyle = "yellowgreen";
-          content.arc(x++,y+=0.3,100,0,Math.PI*2);
-          content.fill();
-          if(x>canvas.width+100 || y>canvas.height+100){
-            x = 50 ; y = 50;
-            // clearInterval(animate);
-          }
-        },1000/60);
-        
-      }
+    methods: {
+        initBall() {
+            let canvas = document.getElementsByClassName('ball-box')[0];
+            let canvasBox = document.getElementById('canvas-ball');
+            let arr = [];
+            let {ballNum} = this;
+            let canvasCon = canvas.getContext('2d');
+            canvas.width = canvasBox.clientWidth;
+            canvas.height = 600;
+            
+            
+            for(let index=0;index<ballNum;index++){
+                let minBall = new ball({ pic: 'ball-box' });
+                arr.push(minBall);
+            }
+            setInterval(() => {
+                canvasCon.clearRect(0,0,canvas.width,canvas.height);
+                for(let item of arr){
+                    item.move();
+                }
+            }, 1000/60);
+
+        }
     }
-  }
+}
 </script>
  
 <style lang="less" scoped>
-  .ball-box{
+.ball-box {
     // width: 100%;
     // height: 600px;
-  }
+}
 </style>
  
