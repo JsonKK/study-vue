8e025c572a958c61b4b450ced14098e6bad6d14c
lsujie <18259002260@163.com>
2020-05-25 21:08:14 +0800

    增加滚动图标

---
+++ vue-travel/src/utils/normalData.js
@@ -0,0 +1,91 @@
+import rowOne1 from '../assets/img/hotel.png';
+import rowOne2 from '../assets/img/jipiao.png';
+import rowOne3 from '../assets/img/train.png';
+import rowOne4 from '../assets/img/package.png';
+import rowOne5 from '../assets/img/piao.png';
+
+import rowTwo1 from '../assets/img/haiwaijiudian.png';
+import rowTwo2 from '../assets/img/flight.png';
+import rowTwo3 from '../assets/img/chuanpiao.png';
+import rowTwo4 from '../assets/img/ziyouxing.png';
+import rowTwo5 from '../assets/img/gonglue.png';
+
+import rowThree1 from '../assets/img/minsu.png';
+import rowThree2 from '../assets/img/zijia.png';
+import rowThree3 from '../assets/img/xinyongka.png';
+import rowThree4 from '../assets/img/tuangou.png';
+import rowThree5 from '../assets/img/yiriyou.png';
+
+const normalData = {
+  // 首页图标
+  touchList : {
+    rowOne : [
+      {
+        title : '酒店',
+        img : rowOne1
+      },
+      {
+        title : '机票',
+        img : rowOne2
+      },
+      {
+        title : '火车票',
+        img : rowOne3
+      },
+      {
+        title : '度假',
+        img : rowOne4
+      },
+      {
+        title : '景点门票',
+        img : rowOne5
+      },
+    ],
+    rowTwo : [
+      {
+        title : '海外酒店',
+        img : rowTwo1
+      },
+      {
+        title : '低价机票',
+        img : rowTwo2
+      },
+      {
+        title : '汽车票船票',
+        img : rowTwo3
+      },
+      {
+        title : '自由行',
+        img : rowTwo4
+      },
+      {
+        title : '攻略',
+        img : rowTwo5
+      },
+    ],
+    rowThree : [
+      {
+        title : '民宿客栈',
+        img : rowThree1
+      },
+      {
+        title : '专车自驾',
+        img : rowThree2
+      },
+      {
+        title : '赚钱-信用卡',
+        img : rowThree3
+      },
+      {
+        title : '旅游团购',
+        img : rowThree4
+      },
+      {
+        title : '一日游',
+        img : rowThree5
+      },
+    ],
+  }
+}
+
+export default normalData;
