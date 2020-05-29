97545bf9810303fdd40e1d00b2d819e10b6aec9d
lsujie <18259002260@163.com>
2020-05-29 09:05:40 +0800

    增加旅游网站图标

---
+++ vue-travel/src/components/search-header.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="header">
+    <div class="left"></div>
+    <div class="content"></div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'search-header',
+    data() {
+      return {
+        
+      }
+    }
+  }
+</script>
+
+<style lang="scss" scoped>
+  
+</style>
---
+++ vue-travel/src/views/search.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="search-wrap"></div>
+</template>
+
+<script>
+  export default {
+    name: 'search',
+    data() {
+      return {
+        
+      }
+    }
+  }
+</script>
+
+<style lang="scss" scoped>
+  
+</style>
