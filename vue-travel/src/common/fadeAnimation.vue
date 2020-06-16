cc2b1c3a5df8e5f28dd4650bf5a942878963127c
lsujie <18259002260@163.com>
2020-06-16 21:59:30 +0800

    旅行网站详情页功能实现

---
+++ vue-travel/src/common/fadeAnimation.vue
@@ -0,0 +1,37 @@
+<template>
+  <transition v-if="show">
+    <slot></slot>
+  </transition>
+</template>
+<script>
+export default {
+  name: 'fadeAnimation',
+  data(){
+    return {
+      show : false
+    }
+  },
+  mounted(){
+    this.initSlotEvent();
+  },
+  methods : {
+    initSlotEvent(){
+      this.$on('hide',this.hide);
+    },
+    //隐藏内容
+    hide(){
+      this.show = false;
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+  .v-enter, .v-leave-to{
+    opacity: 0
+  }
+  .v-enter-active, .v-leave-active{
+    transition: opacity .5s
+  }
+</style>
+ 
---
+++ vue-travel/src/components/detail-list.vue
@@ -0,0 +1,56 @@
+<template>
+  <div class="detail-list">
+    <div class="item-box" v-for="(item,index) in list" :key="index">
+      <div class="item">
+        <span class="item-title-icon"></span>
+        喜喜哈哈
+      </div>
+      <div v-if="item.children" class="item-chilren">
+        <detail-list :list="item.children"></detail-list>
+      </div>
+    </div>
+    
+  </div>
+</template>
+ 
+<script>
+  export default {
+    name: 'detail-list',
+    props:{
+      list : Array
+    },
+    components: {},
+    data(){
+      return{}
+    },
+    mounted(){},
+    computed:{},
+    methods:{},
+    watch:{}
+  }
+</script>
+ 
+<style lang="scss" scoped>
+.item{
+  font-size: 30px;
+  line-height: 3em;
+  height: 3em;
+  border-bottom: 1px solid #ccc;
+}
+
+.item-title-icon{
+  position: relative;
+  left: 6px;
+  top: 6px;
+  display: inline-block;
+  width: 50px;
+  height: 50px;
+  background: url(http://s.qunarzz.com/piao/image/touch/sight/detail.png) 0 -67px no-repeat;
+  margin-right: 10px;
+  vertical-align: -10px;
+}
+.item-chilren{
+  padding: 0 1em;
+}    
+</style>
+ 
